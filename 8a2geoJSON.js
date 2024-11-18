289a59eb8b37524304e40e814029646092312165
vikasagartha@gmail.com <vikasagartha@gmail.com>
2024-11-17 19:32:28 -0800

    fix 2 arrays to object logic.

--- 8a2geoJSON.js
+++ 8a2geoJSON.js
@@ -1,10 +1,13 @@
 const fs = require('fs')
 console.log('welcome. converting 8a csv to geoJSON----------')
 
-const readCSV = (data) => {
-   return data.split('/\r\n|\n/').map(d => d.split(','))
+const csvToFeatures = (csv) => {
+   const [r1, ...rest] = csv.split(/\r?\n/)
+   const keys = r1.split(',')
+   const features = rest.map(d => d.split(','))
+   return features.map(values => keys.reduce((o, k, i) => ({...o, [k]: values[i]}), {}))
 }
 fs.readFile('vikas-8a.csv', "utf8", (err, inputD) => {
    if (err) throw err;
-   console.log(readCSV(inputD))
+   console.log(csvToFeatures(inputD))
 })
