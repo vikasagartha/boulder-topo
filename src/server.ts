eb5f5dd2bcd76d40ad8edebffd8061c7094855d8
vikasagartha@gmail.com <vikasagartha@gmail.com>
2024-12-30 19:19:37 -0800

    catching error from python

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import {writeFile} from 'fs'
 import express from 'express'
 import multer from 'multer'
+import { spawn } from 'child_process'
 import {buildQueryFile} from './backend/index'
 
 const app = express()
@@ -38,12 +39,40 @@ app.post('/upload', upload.single('file'), async (req: express.Request, res: exp
 
   const dataPath = `${__dirname}/data/${req.file.filename}`
   const queryPath = await buildQueryFile(dataPath)
+  const pythonPath = process.env.PYTHON_BINARY
 
   if(queryPath instanceof Error) {
     res.status(400).send({message: 'Your data was successfully uploaded, but there was an error building a query file for geocoding your data. Please contact admin: vikasagartha@gmail.com'});
     return
   }
-  res.json({ message: 'Query file generated!', dataPath, queryPath});
+
+  if(!pythonPath || typeof pythonPath !== 'string') {
+    res.status(400).send({message: 'Your data was successfully uploaded, but there was an error but ther was an error intitializing the python environment. Please contact admin: vikasagartha@gmail.com'});
+    return
+  }
+
+  const pyProg = await spawn(pythonPath, ['./batch_geocoder.py', queryPath])
+
+  pyProg.stdout.on('data', function(data) {
+    console.log('stdout: ' + data);
+  });
+
+  pyProg.stderr.on('data', (data) => {
+    console.log('stderr: ' + data);
+  });
+
+  pyProg.on('close', code => {
+    console.log(`child process exited with code ${code}`);
+    if(code !== 0){
+      res.status(400).send({
+        message: 'There was an error geocoding your data. Please contact admin: vikasagartha@gmail.com',
+        code
+      });
+    }
+  })
+
+  //res.json({ message: 'Query file generated, python sourced!', dataPath, queryPath, output, errorOutput})
+  //setTimeout(() => res.json({ message: 'Query file generated, python sourced!', dataPath, queryPath}), 5000)
 
 });
 
